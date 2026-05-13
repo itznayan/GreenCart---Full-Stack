77a4a220f0b0c203611a9e564ee68a1ac2de8297
Nayan Mahera <maheranayan18@gmail.com>
2026-05-13 13:55:06 +0530

    fixed updated fronend backend connectes

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+axios.defaults.baseURL = "https://greencart-full-stack.onrender.com";
 
 export const AppContext = createContext();
 
